df4aa17377953a1d43a7ff150f2e666c48be376d
Dale TerrelL <tragicid@#gmail.com>
2020-07-10 03:06:26 -0500

    again

--- src/main/java/com/aim/movie/App.java
+++ src/main/java/com/aim/movie/App.java
@@ -1,13 +1,47 @@
 package com.aim.movie;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class App {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/movie";
+    private static final String USER = "dale terrell";
+    private static final String PASS = "Misery88?";
+
+    public static void main( String[] args ) {
+
+        try {
+
+            Scanner input = new Scanner(System.in)
+            System.out.println("Please enter a movie title: ");
+            String movieTitle = input.nextLine();
+            input.close();
+
+            String sql = "select movie_name, first_name, last_name from movies m "
+                    + "join directors d on d.director_id on m.director_id ",
+
+            StringBuilder sql2 = new StringBuilder();
+            sql2.append("select movie_name, first)name, last_name ");
+            sql2.append("from movies m ");
+            sql2.append("join direcctors d on director_id = m.director_id ");
+            sql2.append("join reatings r on r.rating_")
+            )        
+
+            Connection connection = DriverManager.getConnection(URL, USER, PASS);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from movies;");
+
+            while(resultSet.next()) {
+                String movieTitle = resultSet.getString("movie_name");
+                System.out.println("Movie Title: " + movieTitle);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
