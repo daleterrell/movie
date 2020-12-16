c498d71375ba30fa2f3e3c1fad7d63b48a1568eb
Dale Terrell <tragicid@gmail.com>
2020-12-16 15:19:14 -0600

    running

--- AimUnit4JDBC/src/main/java/com/aim/movie/App.java
+++ AimUnit4JDBC/src/main/java/com/aim/movie/App.java
@@ -1,13 +1,32 @@
 package com.aim.movie;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static String URL = "jdbc:mysql://localhost:3306/movie1";
+    private static final String USER = "root";
+    private static final String PASS = "Misery88?";
+
+    public static void main(String[] args) {
+
+        try {
+            Connection connection = DriverManager.getConnection(URL, USER, PASS);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from movies");
+
+            while (resultSet.next()) {
+                String movieTitle = resultSet.getString("movie_name");
+                System.out.println("Movie Title " + movieTitle);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
